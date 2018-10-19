2d1ef57b9b02a512fe2e9b9142a188a88e54e316
Salvador Briseno <s_salvador.briseno@ousd.org>
2018-10-19 01:28:51 +0000

    update

--- js/script.js
+++ js/script.js
@@ -4,8 +4,62 @@
 
 //GLOBAL VARIABLES
 /* global $ */
-
+var items = ["Rock","Paper","Scissors"]
 
 
 // DOCUMENT READY FUNCTION BELOW
+$("#shoot").click(function()
+{
+    
+    var compC = items[Math.floor(Math.random()*items.length)];
+
+    var userC = $("#input").val();
+    
+    $("#computerChoice").text(compC);
+    
+    $("#userChoice").text(userC);
+    
+if(userC !== "Rock" && userC !== "Scissors" && userC !== "Paper")
+{
+    $("#result").text("invalid option");
+}
+    
+if(userC === "Rock" && compC === "Paper")
+{
+    $("#result").text("Computer Wins");
+}
+if(userC === "Scissors" && compC === "Paper")
+{
+     $("#result").text("User Wins");
+}
+if(userC === "Scissors" && compC === "Rock")
+{
+     $("#result").text("Computer Wins");
+}
+if(userC === "Scissors" && compC === "Scissors")
+{
+     $("#result").text("No one Wins");
+}
+if(userC === "Paper" && compC === "Paper")
+{
+     $("#result").text("No one Wins");
+}
+if(userC === "Paper" && compC === "Rock")
+{
+     $("#result").text("User Wins");
+}
+if(userC === "Paper" && compC === "Scissors")
+{
+     $("#result").text("Computer Wins");
+}
+if(userC === "Rock" && compC === "Scissors")
+{
+     $("#result").text("User Wins");
+}
+if(userC === "Rock" && compC === "Rock")
+{
+     $("#result").text("No one Wins");
+}
+});
+
 
